be64c4b469f8eab35ed57e60660c1a8b469fb785
spunkyone <122656406+spunkyone@users.noreply.github.com>
2024-04-04 03:24:59 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 require('@babel/register');
-require('ts-node').register({ transpileOnly: false });
+require('ts-node').register({ transpileOnly: false; false });
 
 require('./helpers/setup-helper');
 
