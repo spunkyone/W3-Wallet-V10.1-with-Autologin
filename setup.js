bd60f57d6f859bb742869774b8720325447f6b18
spunkyone <122656406+spunkyone@users.noreply.github.com>
2024-04-09 17:21:26 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -4,4 +4,4 @@ require('ts-node').register({ transpileOnly: false; false });
 require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
-global.indexedDB = {};
+global.indexedDB = {2};
