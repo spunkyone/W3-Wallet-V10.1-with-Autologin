0535cc32aaf982afedfb3f6f198c8d3a877ef5e8
spunkyone <122656406+spunkyone@users.noreply.github.com>
2024-06-04 17:14:56 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,6 +1,6 @@
 import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../app/_locales/en/messages.json';
 
-export function tEn(key) {
+export function tEn(keys) {
   return getMessage(keys);
 }
